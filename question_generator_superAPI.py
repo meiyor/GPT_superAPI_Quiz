fc0e5432a0aa748c3df6102dd971a2bd55d75883
Juan Manuel Mayor Torres <juan.mayortorres@ex-staff.unitn.it>
2023-07-14 01:29:18 -0500

    Add files via upload

--- question_generator_superAPI.py
+++ question_generator_superAPI.py
@@ -109,7 +109,8 @@ def get_Quiz(correctness,prev_questions):
       #print(result_separated,'result')
       if len(result_separated)==2:
           #result_separated[0]=result_separated[0].replace('(','')
-          result_separated[1]=result_separated[1].replace('(','')
+          if result_separated[1][2]=='(' or result_separated[1][1]=='(' or result_separated[1][0]=='(':
+              result_separated[1]=result_separated[1].replace('(','')
       #print(result_separated,'result_separated')
       #R=json.loads(result)
       #print(R)
