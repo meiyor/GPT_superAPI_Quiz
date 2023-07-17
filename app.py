4642e6820a3881f1a8a3e228edb0cce001f05bb8
Juan Manuel Mayor Torres <juan.mayortorres@ex-staff.unitn.it>
2023-07-17 03:32:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def predict():
     if not(text.lower() == 'yes') and not(text.lower() == 'y') and not(text.lower() == 'ok') and not(text.lower() == 'ye') and not(text.lower() == 'yeah') and len(text)<=3 and not(text.lower() == 'no') and not(text.lower() == 'n') and (text[0].lower() == 'a' or text[0].lower() == 'b' or text[0].lower() == 'c' or text[0].lower() == 'd' or text[0].lower() == 'e'):
        count_questions=count_questions+1
     
-    if (text.lower() == 'yes' or text.lower() == 'y' or text.lower() == 'ok' or text.lower() == 'ye' or text.lower() == 'yeah') and count_questions>=number_questions:
+    if (text.lower() == 'yes' or text.lower() == 'y' or text.lower() == 'ok' or text.lower() == 'ye' or text.lower() == 'yeah') and count_questions>=number_questions and len(username)>=0 and len(password)>=0:
           ## check for ids
           for value in db.session.query(gpt_data.id).distinct():
                 ids.append(value[0])
@@ -193,6 +193,10 @@ def predict():
           ids = []
           len_quiz=number_questions
           message =  {"answer": f"Quiz is ready! Want to start the {len_quiz} questions? reply yes or no!"}
+          print(message,'message')
+          return jsonify(message)
+    else:
+          message =  {"answer": "Username and Password are empty! can't add it into the database"}
           print(message,'message')
           return jsonify(message)
 
