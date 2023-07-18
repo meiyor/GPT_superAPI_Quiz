be20eadb8ed7475cfe72b3eb12c3c1d452e0b45d
Juan Manuel Mayor Torres <juan.mayortorres@ex-staff.unitn.it>
2023-07-18 03:01:07 -0500

    Add files via upload

--- question_generator_superAPI.py
+++ question_generator_superAPI.py
@@ -39,30 +39,9 @@ def get_Quiz(correctness,prev_questions):
            prev_question_string = prev_question_string+' or -'+body_question[0]+'\n'
   #prev_question_string = prev_question_string+'\n'
   print(prev_question_string,'prev_question_string')
-
-  ## uncomment this if you want to make it a bit faster after you are accumulating questions
-  ##if correctness == 0:
-  ##   number_query=random.randint(0,20)
-  ##   if (number_query % 2) == 0:
-  ##       if number_query<=10:
-  ##         data = f'Write a (ONE) NEW question about general culture with {number_options} choices/answers specified with letters after the question, specify the correct answer at the end of the text \n'+'and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ##       else:
-  ##         data = f'Write a (ONE) NEW, easy, and random question with  {number_options} choices/answers, specify the choices with letters after the question, specify the correct answer at the end of the text \n'+' and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ##   else:
-  ##         data = f'Write a (ONE) NEW question about contemporary culture with {number_options} choices/answers specified with letters after the question, specify the correct answer at the end of the text \n'+'and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ##else:
-  ##   number_query=random.randint(0,20)
-  ##   if (number_query % 2) == 0:
-  ##     if number_query<=10:
-  ##       data =  f'Write a (ONE) NEW hard/difficult question about any specific topic with {number_options} choices/answers specified with letters after the question, specify the correct answer at the end of the text \n'+'and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ##     else:
-  ##       data = f'Write a (ONE) NEW hard/difficult random question {number_options} choices/answers about any specific topic, specify the choices with letters after the question, specify the correct answer at the end of the text \n'+'and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ##   else:
-  ##       data =  f'Write a (ONE) VERY HARD! NEW question about any specific topic with {number_options} choices/answers specified with letters after the question, specify the correct answer at the end of the text \n'+'and please DO NOT repeat any of these following questions: '+ prev_question_string+ '\n'
-  ## response = requests.post(url, headers=headers, data=data)  
-
-  ## Adapts difficulty with two different types of queries grouping the previous question for no repeating a new question again for each session.
-  ## if the previous question was answered correctly the difficult query is activated and more complicated topics are queried to SuperAPI
+ 
+  ## combine the queries depending on a random number
+  number_selection = random.randint(0,1000)
   number_options=random.randint(3,5)
   data1 = 'DO NOT WRITE or REPEAT ANY of these following questions:'+ prev_question_string+'\n'
   if correctness == 0:
@@ -83,23 +62,34 @@ def get_Quiz(correctness,prev_questions):
          data2 = f'Write a NEW hard/difficult random question {number_options} choices/answers about any specific topic, specify the choices with letters after the question, specify the correct answer at the end of the text \n'
      else:
          data2 =  f'Write a NEW VERY HARD! question about any specific topic with {number_options} choices/answers specified with letters after the question, specify the correct answer at the end of the text \n'
+  
+  if (number_selection % 2) == 0:
+      ## requests variation depending on the random number
+      for attemp in range(10):
+        try:
+            response_ack = requests.post(url, headers=headers, json=data_model, data=data1.encode('utf-8').decode('utf-8'))
+            time.sleep(1)
+            break
+        except requests.exceptions.ChunkedEncodingError:
+            time.sleep(1)
 
-  #for attemp in range(10):
-  #  try:
-  #      response_ack = requests.post(url, headers=headers, json=data_model, data=data1.encode('utf-8').decode('utf-8'))
-  #      time.sleep(1)
-  #      break
-  #  except requests.exceptions.ChunkedEncodingError:
-  #      time.sleep(1)
-
-  #print(response_ack,'response_ack')
-  for attemp in range(10):
-    try:
-        response = requests.post(url, headers=headers, json=data_model, data=data1.encode('utf-8').decode('utf-8')+data2.encode('utf-8').decode('utf-8'))
-        time.sleep(1)
-        break
-    except requests.exceptions.ChunkedEncodingError:
-        time.sleep(1)
+      for attemp in range(10):
+        try:
+            response_ack = requests.post(url, headers=headers, json=data_model, data=data2.encode('utf-8').decode('utf-8'))
+            time.sleep(1)
+            break
+        except requests.exceptions.ChunkedEncodingError:
+            time.sleep(1)
+      print(response_ack,'response_ack')
+  else:
+      ## requests variation depending on the random number
+      for attemp in range(10):
+        try:
+            response = requests.post(url, headers=headers, json=data_model, data=data1.encode('utf-8').decode('utf-8')+data2.encode('utf-8').decode('utf-8'))
+            time.sleep(1)
+            break
+        except requests.exceptions.ChunkedEncodingError:
+            time.sleep(1)
 
   ## if the fecth is done correctly process the message from the chatbox
   if response.status_code == 200:
